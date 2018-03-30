075c04148931d00fa01e6133a0de5b689664bbc2
Noemi Romero <nlll02@yahoo.com>
2018-03-30 13:55:31 -0500

    created the array of words

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 console.log('My game')
+
+
+$('#name').on('click', (event) => {
+	console.log('Hello')
+	const changeName = $('input').val();
+	
+	//$('#display').empty();
+	$('#display').text(changeName);
+	
+	//this.name = changeName;
+})
+const arrayOfWords = ['yellow', 'one', 'three', 'apple', 'the',
+ 'array', 'house',
+ 'pizza', 'tea', 
+ 'thirteen', 'monkey', 'banana', 'awesome', 'incredicle', 
+ ' strengths', 'a', 'cat', 'tiger', 'elephant', 'dinosaur', 'day', 'is', 'good'];
+console.log(arrayOfWords);
+
+const word = arrayOfWords[Math.floor(Math.random() * arrayOfWords.length)];
