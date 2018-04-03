a9c30248470bf60016e192c1d5dbb11bf19efc7a
Noemi Romero <nlll02@yahoo.com>
2018-04-03 15:19:31 -0500

    reorganizing and fixed the modal

--- js/app.js
+++ js/app.js
@@ -1,25 +1,15 @@
 console.log('My game')
 
-let time = 20;
+let time = 60;
 let timer;
 let wordCount = 0;
 let points = 0;
 // this keeps track of which we are on
 let nextRound = 0;
 
-$('#name').on('click', (event) => {
-	event.preventDefault();
-	// $('#name').effect('explode');
-	console.log('Hello')
-	const changeName = $('input').val();
-	//$('#display').empty();
-	$('#display').text(changeName);	
-	setTimer();
-})
 const words = [
 	// ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
- 	['yellow', 'one', 'blue', 'green', 'black', 'two', 'three', 'apple', 'the', 'array', 'house', 'pizza', 'tea',  'monkey', 'ten', 'car', 'cup', 'plate',
- 	'candy', 'banana', 'awesome', 'horse', 'strengths', 'a', 'cat', 'tiger',  'day', 'is', 'good', 'so', 'some', 'sound', 'still', 'such', 'take', 'tell', 'than', 'commit', 'bird'],
+ 	['yellow', 'one', 'blue', 'green', 'black', 'two', 'three', 'apple', 'the', 'array', 'house', 'pizza', 'tea',  'monkey', 'ten', 'car', 'cup', 'plate', 'candy', 'banana', 'awesome', 'horse', 'strengths', 'a', 'cat', 'tiger',  'day', 'is', 'good', 'so', 'some', 'sound', 'still', 'such', 'take', 'tell', 'than', 'commit', 'bird'],
  	['Alfa', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliett', 'Kilo', 'Lima', 'Mike', 'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey', 'X-ray', 'Yankee', 'Zulu'],
  	['worksheet', 'dinosaur', 'printing', 'incredible', 'thirteen', 'vocabulary', 'triangle', 'rhyming', 'examples', 'dictionary',  'something', 'intend', 'practice', 'pictures', 'poems', 'selections', 'elephant', 'giraffe', 'coffee', 'address', 'evident', 'accord', 'approach', 'establish', 'straight', 'apparent', 'passage'],
 	['traintrack', 'continuing', 'outstanding', 'appointed', 'earnest', 'convention', 'territory', 'undertake', 'majority', 'attitude', 'manifest', 'resource', 'contempt', 'distinction', 'inclined', 'attribute', 'disposition', 'bestow', 'corruption', 'crerical'],
@@ -27,22 +17,24 @@ const words = [
 ] 
 
 
-console.log(words)
-const checkWord = () => {
 
+const checkWord = () => {
+	// increase wordCount
 	if(wordCount < 10){
 		wordCount++;
 		console.log(wordCount);
-	} else {
-		console.log('hitting')
+
+	} else { // we've done 10 words -- reset wordCount to 0 and go to next round
+
+		// console.log('hitting')
 		wordCount = 0;
-		nextRound++;
-		setTimer();
-		console.log(nextRound, ' this is nextRound')
-		$('#rounds').text('Round: ' + nextRound)
+		startRound();
+		// nextRound++;
+		// setTimer();
+		// console.log(nextRound, ' this is nextRound')
+		
 	}
 }
-
 
 
 
@@ -50,17 +42,18 @@ const checkWord = () => {
 const getWord = () => {
 
 	// get the correct array for this round
-	const roundArray = words[nextRound];
-	console.log(roundArray)
+	const roundArray = words[nextRound-1];
+	// console.log(roundArray)
 
 	// get random word from that array
  	const word = roundArray[Math.floor(Math.random() * roundArray.length)] 	
- 	console.log(word)
+ 	// console.log(word)
 
 
   	let box = $('#wordBox')
   	box.text(word);
-  	console.log(word)
+  	// console.log(word)
+
 	///this will change the input value to null
 	// slice whatever word you got out of the roundOneWords
 	
@@ -68,30 +61,117 @@ const getWord = () => {
 
   	
 }
+// console.log(getWord);
+// getWord();
 
-getWord();
 
-$('#answer').on('click', (event) => {//this gets the input 
-	event.preventDefault();//this prevents it 
-	console.log('response');
 
-	const wordAnswer = $('#answerText').val();
+const setTimer = () => {
+	
+  	timer = setInterval(() => {
+  		console.log(timer+" is this");
+
+		time--
+		$('#time').text('Time: ' + time)
+		console.log( time + ' timer is running');
+
+		if(time === 0){
+
+			// console.log('is this happening iddkkkk')
+	 		
+			clearInterval(timer);
+			
+			toggleModal();
+			// startRound();
+		
+			// Try to create a modal 
+			// Your score was blah blah,
+			// are you ready for round two
+			// then click button yes
+
+			// when click that button this starts
+			
+			// time = 10;
+			// console.log(nextRound, ' this is nextRound')
+			// $('#rounds').text('Round: ' + nextRound)
+
+			// toggleModal();
+			// clearInterval(timer);
+			// // wordCount = 0;
+			// // nextRound++;
+			// setTimer();
+			// getWord();
+			// print message in html
+			console.log("Time's up!")
+
+			// if they got enough points, move to next round
+			// setTimer()
+		}
+
+	    
+	}, 1000);
+}
 	
 
-	$('#answerText').text(wordAnswer);
-	console.log(wordAnswer);
+// // rewrite this code in jquery
+// // const modal = document.querySelector(".modal");
+// const modal = $(".modal");
 
-	if($('#wordBox').text() === wordAnswer) {//if the word given is the same
+// // const trigger = document.querySelector(".trigger");
+// const trigger = $(".trigger");
+
+// // const closeButton = document.querySelector(".close-button");
+// const closeButton = $(".close-button");
+
+function toggleModal() {
+	// how do I toggle a class in jquery
+    $(".modal").toggleClass("show-modal");
+}
+
+function startRound() {
+	if(nextRound !== 0) {
+		toggleModal();
+	}	
+	setTimer();
+	nextRound++;
+	$('#rounds').text('Round: ' + nextRound)
+	getWord();
+	time = 60;
+
+}
+// const 
+
+//click functions in jquery
+// trigger.addEventListener("click", toggleModal);
+$("#modal-button").on("click", startRound);
+
+// closeButton.addEventListener("click", toggleModal);
+$(".close-button").on("click", toggleModal);
+// window.addEventListener("click", windowOnClick);
+
+// check answer
+$('#answer').on('click', (event) => {//this gets the input 
+	event.preventDefault();//this prevents it 
+	// console.log('response');
+
+	const wordAnswer = $('#answerText').val();
+
+	$('#answerText').text(wordAnswer);
+	// console.log(wordAnswer);
+
+	//if the word given is the same
+	if($('#wordBox').text() === wordAnswer) {
 		points++;
 		// print points in html
 		$('#score').text("Points: " + points)
-		console.log(points)
-		console.log('match');//this will pop an alert if you get it right
+		// console.log(points)
+		// console.log('match');//this will pop an alert if you get it right
 		getWord();	
 		checkWord();
 		// we can clear out the input
 		$('#answerText').val('')
-	} else {
+
+	} else { // user typed word incorrectly
 		points--;
 		$('#score').text("Points: " + points)
 		// print points in html
@@ -103,84 +183,20 @@ $('#answer').on('click', (event) => {//this gets the input
 	}
 })
 
-const setTimer = () => {
-	
-  	timer = setInterval(() => {
-  		console.log(timer+" is this");
+$('#name').on('click', (event) => {
+	event.preventDefault();
+	// $('#name').effect('explode');
+	console.log('Hello')
+	const changeName = $('input').val();
+	//$('#display').empty();
+	$('#display').text(changeName);	
 
-	time--
-	$('#time').text('Time: ' + time)
-	console.log( time + ' timer is running');
-
-	if(time === 0){
-
-		console.log('is this happening iddkkkk')
- 		
-		clearInterval(timer);
-		
-		toggleModal();
-		startRound();
-		// Try to create a modal 
-		// Your score was blah blah,
-		// are you ready for round two
-		// then click button yes
-
-		// when click that button this starts
-		
-		// time = 10;
-		// console.log(nextRound, ' this is nextRound')
-		// $('#rounds').text('Round: ' + nextRound)
-
-		// toggleModal();
-		// clearInterval(timer);
-		// // wordCount = 0;
-		// // nextRound++;
-		// setTimer();
-		// getWord();
-		// print message in html
-		console.log("Time's up!")
-
-		// if they got enough points, move to next round
-		// setTimer()
-	}
-
-    
-  }, 1000);
-}
-	
-
-	// rewrite this code in jquery
- 	// const modal = document.querySelector(".modal");
- 	const modal = $(".modal");
-
-    // const trigger = document.querySelector(".trigger");
-    const trigger = $(".trigger");
-
-    // const closeButton = document.querySelector(".close-button");
-    const closeButton = $(".close-button");
-
-    function toggleModal() {
-    	// how do I toggle a class in jquery
-        $(".modal").toggleClass("show-modal");
-    }
-
- 
-    //click functions in jquery
-    // trigger.addEventListener("click", toggleModal);
-    $("#modal-button").on("click", toggleModal);
-
-    // closeButton.addEventListener("click", toggleModal);
-    $(".close-button").on("click", toggleModal);
-    // window.addEventListener("click", windowOnClick);
-	
+	// setTimer();
+	startRound();
+})
 
 // setTimer();
-const startRound = () => {
-	setTimer();
-	getWord();
-	time = 20;
 
-}
 
 
 
