9f1b8f110b350f5369808b62ac0a10316d3f2e16
Noemi Romero <nlll02@yahoo.com>
2018-04-04 12:36:07 -0500

    created the X

--- js/app.js
+++ js/app.js
@@ -6,9 +6,12 @@ let wordCount = 0;
 let points = 0;
 // this keeps track of which we are on
 let nextRound = 0;
+let correctWord = 0;
+let wrongAnswer = 0;
 
 const words = [
 	// ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+ 	['red', 'blue', 'green', 'black', 'purple', 'pink', 'orange', 'gray', 'white', 'brown'],
  	['yellow', 'one', 'blue', 'green', 'black', 'two', 'three', 'apple', 'the', 'array', 'house', 'pizza', 'tea',  'monkey', 'ten', 'car', 'cup', 'plate', 'candy', 'banana', 'awesome', 'horse', 'strengths', 'a', 'cat', 'tiger',  'day', 'is', 'good', 'so', 'some', 'sound', 'still', 'such', 'take', 'tell', 'than', 'commit', 'bird'],
  	['Alfa', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliett', 'Kilo', 'Lima', 'Mike', 'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey', 'X-ray', 'Yankee', 'Zulu'],
  	['worksheet', 'dinosaur', 'printing', 'incredible', 'thirteen', 'vocabulary', 'triangle', 'rhyming', 'examples', 'dictionary',  'something', 'intend', 'practice', 'pictures', 'poems', 'selections', 'elephant', 'giraffe', 'coffee', 'address', 'evident', 'accord', 'approach', 'establish', 'straight', 'apparent', 'passage'],
@@ -18,11 +21,11 @@ const words = [
 
 
 
-const checkWord = () => {
+const countingWords = () => {
 	// increase wordCount
 	if(wordCount < 10){
 		wordCount++;
-		console.log(wordCount);
+		// console.log(wordCount.length);
 
 	} else { // we've done 10 words -- reset wordCount to 0 and go to next round
 		console.log("resetting word count")
@@ -31,7 +34,7 @@ const checkWord = () => {
 		toggleModal();		
 	}
 }
-
+// console.log(countingWords);
 const getWord = () => {
 
 	// get the correct array for this round
@@ -45,6 +48,23 @@ const getWord = () => {
 	
 	// tell user they won (html/jq)  	
 }
+const correctGuess = () => {
+	if(correctWord === 10){
+		startRound();
+	}
+	else if(correctWord === 7){
+		gameOver();
+	} else {
+		console.log(correctWord);
+	}
+
+}
+const incorrectGuess = () => {
+	let x = $('#strike')
+	x.text(wrongAnswer);
+}
+
+console.log(wrongAnswer);
 
 const setTimer = () => {
 	
@@ -63,12 +83,7 @@ const setTimer = () => {
 			
 			toggleModal();
 		
-			// Try to create a modal 
-			// Your score was blah blah,
-			// are you ready for round two
-			// then click button yes
-			// when click that button this starts
-
+			
 			// print message in html
 			console.log("Time's up!")
 
@@ -77,6 +92,11 @@ const setTimer = () => {
 
 	}, 1000);
 }
+ 			// Try to create a modal 
+			// Your score was blah blah,
+			// are you ready for round two
+			// then click button yes
+			// when click that button this starts
 
 function toggleModal() {
 	// how do I toggle a class in jquery
@@ -92,7 +112,14 @@ function startRound() {
 	$('#rounds').text('Round: ' + nextRound)
 	getWord();
 	time = 60;
+	correctWord = 0;
 
+}
+function gameOver() {
+	if(wrongAnswer === 3){
+	clearInterval(timer);
+	alert('GAME OVER')
+	}
 }
 
 
@@ -110,28 +137,36 @@ $('#answer').on('click', (event) => {//this gets the input
 	// console.log('response');
 
 	const wordAnswer = $('#answerText').val();
-
+	console.log(wordAnswer);
 	$('#answerText').text(wordAnswer);
 	// console.log(wordAnswer);
 
 	//if the word given is the same
 	if($('#wordBox').text() === wordAnswer) {
 		points++;
+		correctWord++;
+		console.log(correctWord);
 		// print points in html
 		$('#score').text("Points: " + points)
 		getWord();	
-		checkWord();
+		// countingWords();
 		// clear out the input
 		$('#answerText').val('')
 
 	} else { // user typed word incorrectly
 		points--;
+		wrongAnswer++;
+		// x.text(wrongAnswer)
+		alert(wrongAnswer + ' oh man');
+		gameOver();
+		
+		
 		$('#score').text("Points: " + points)
 		// print points in html
-		console.log(points)
-		alert("keep trying")
-		getWord();
-		checkWord();
+		// console.log(points)
+		// alert("keep trying")
+		// getWord();
+		// countingWords();
 		$('#answerText').val('')
 	}
 })
@@ -141,6 +176,7 @@ $('#name').on('click', (event) => {
 	// $('#name').effect('explode');
 	const changeName = $('input').val();
 	$('#display').text(changeName);	
+	$('#playername').val('')
 	startRound();
 })
 
@@ -157,14 +193,15 @@ $('#name').on('click', (event) => {
 
 // .effect('explode')
 	
-	// $('#wordBox').empty();
-// make counter to keep track of how many words so far
+	
 
-// each time you get aword 
+	// make counter to keep track of how many words so far
+
+	// each time you get aword 
 	// if the counter is 10
-		// next round
+	// next round
 	//otherwise increment the counter
-		// get the next word (what you already have)
-///keep track of thw word
-//point at
+	// get the next word (what you already have)
+	///keep track of thw word
+	//point at
 
