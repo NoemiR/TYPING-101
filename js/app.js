974b0819815335c312c5249f7596a5b481d5d5c5
Noemi Romero <nlll02@yahoo.com>
2018-04-05 11:51:48 -0500

    cleaned up the code

--- js/app.js
+++ js/app.js
@@ -4,29 +4,42 @@ let time = 60;
 let timer;
 let wordCount = 0;
 let points = 0;
-// this keeps track of which we are on
 let nextRound = 0;
 let correctWord = 0;
 let wrongAnswer = 0;
 let text = $("#strike")
 text.css("color: red", "font-size: 45px");
-// console.log()
-	// text = 0;
-// let	textt = (text + "x");
-// let struck = $("#strike").text(textt);
-console.log(text)
 
-const words = [
-	['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
- 	['red', 'is', 'this', 'and', 'cold', 'total', 'food', 'blue', 'so', 'green', 'car', 'black', 'purple', 'pink', 'orange', 'gray', 'white', 'brown', 'aqua', 'pig', 'tea', 'bed', 'giant', 'small'],
- 	['yellow', 'one', 'blue', 'green', 'black', 'two', 'three', 'apple', 'the', 'array', 'house', 'pizza', 'tea',  'monkey', 'ten', 'car', 'cup', 'plate', 'candy', 'banana', 'awesome', 'horse', 'strengths', 'carrot', 'cat', 'tiger',  'day', 'string', 'good', 'soap', 'some', 'sound', 'still', 'such', 'take', 'tell', 'than', 'commit', 'bird'],
- 	['rainbow', 'raining', 'thunder', 'equal', 'telephone', 'resume', 'computer', 'option', 'price', 'premium']
- 	
- 	['worksheet', 'dinosaur', 'printing', 'incredible', 'thirteen', 'vocabulary', 'triangle', 'rhyming', 'examples', 'dictionary',  'something', 'intend', 'practice', 'pictures', 'poems', 'selections', 'elephant', 'giraffe', 'coffee', 'address', 'evident', 'accord', 'approach', 'establish', 'straight', 'apparent', 'passage'],
-	['traintrack', 'continuing', 'outstanding', 'appointed', 'earnest', 'convention', 'territory', 'undertake', 'majority', 'attitude', 'manifest', 'resource', 'contempt', 'distinction', 'inclined', 'attribute', 'disposition', 'bestow', 'corruption', 'crerical'],
-	['ascertain', 'perpetual', 'substancial', 'elaborate', 'conspicuous', 'proceeding', 'extravagant', 'venerate', 'suffrage', 'intrigue', 'dispatch', 'railroad', 'undertaking', 'predecessor', 'delicacy'],
-	['Alfa', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliett', 'Kilo', 'Lima', 'Mike', 'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey', 'X-ray', 'Yankee', 'Zulu'],
-]; 
+
+
+const words = [    
+//  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+// 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+['red', 'is', 'this', 'and', 'cold', 'total', 'food', 'blue', 'so', 'green',
+'car', 'black', 'purple', 'pink', 'orange', 'gray', 'white', 'brown', 'aqua',
+'pig', 'tea', 'bed', 'giant', 'small'],     
+['yellow', 'one', 'blue', 'green',
+'black', 'two', 'three', 'apple', 'the', 'array', 'house', 'pizza', 'tea',
+'monkey', 'ten', 'car', 'cup', 'plate', 'candy', 'banana', 'awesome', 'horse',
+'strengths', 'carrot', 'cat', 'tiger',  'day', 'string', 'good', 'soap',
+'some', 'sound', 'still', 'such', 'take', 'tell', 'than', 'commit', 'bird'],
+['rainbow', 'raining', 'thunder', 'equal', 'telephone', 'resume', 'computer',
+'option', 'price', 'premium'],	
+['worksheet', 'dinosaur', 'printing', 'incredible', 'thirteen',
+'vocabulary', 'triangle', 'rhyming', 'examples', 'dictionary',  'something',
+'intend', 'practice', 'pictures', 'poems', 'selections', 'elephant',
+'giraffe', 'coffee', 'address', 'evident', 'accord', 'approach', 'establish',
+'straight', 'apparent', 'passage'],     ['traintrack', 'continuing',
+'outstanding', 'appointed', 'earnest', 'convention', 'territory', 'undertake',
+'majority', 'attitude', 'manifest', 'resource', 'contempt', 'distinction',
+'inclined', 'attribute', 'disposition', 'bestow', 'corruption', 'crerical'],
+['ascertain', 'perpetual', 'substancial', 'elaborate', 'conspicuous',
+'proceeding', 'extravagant', 'venerate', 'suffrage', 'intrigue', 'dispatch',
+'railroad', 'undertaking', 'predecessor', 'delicacy'],     ['Alfa', 'Bravo',
+'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliett',
+'Kilo', 'Lima', 'Mike', 'November', 'Oscar', 'Papa', 'Quebec', 'Romeo',
+'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey', 'X-ray', 'Yankee', 'Zulu'],
+];
 
 
 
@@ -34,7 +47,7 @@ const countingWords = () => {
 	// increase wordCount
 	if(wordCount < 10){
 		wordCount++;
-		// console.log(wordCount.length);
+		
 
 	} else { // we've done 10 words -- reset wordCount to 0 and go to next round
 		console.log("resetting word count")
@@ -43,7 +56,7 @@ const countingWords = () => {
 		toggleModal();		
 	}
 }
-// console.log(countingWords);
+
 const getWord = () => {
 
 	// get the correct array for this round
@@ -71,13 +84,6 @@ const correctGuess = () => {
 	}
 
 }
-// const incorrectGuess = () => {
-// 	let x = $('#strike')
-
-// }
-
-	
-// console.log(wrongAnswer);
 
 
 function startRound() {
@@ -97,7 +103,6 @@ function startRound() {
 function gameOver() {
 	if(wrongAnswer === 3){
 		clearInterval(timer);
-		// alert('GAME OVER')
 		$('#wordBox').text("")
 		$('#answerText').hide()
 		$('#answer').hide()
@@ -121,16 +126,9 @@ const setTimer = () => {
 
 		if(time === 0){
 
-			// console.log('is this happening iddkkkk')
-	 		
 			clearInterval(timer);
 			
 			toggleModal();
-		
-			
-			// print message in html
-			console.log("Time's up!")
-
 			// if they got enough points, move to next round
 		}
 
@@ -141,13 +139,6 @@ function toggleModal() {
 	// how do I toggle a class in jquery
     $(".modal").toggleClass("show-modal");
 }
-			// Try to create a modal 
-			// Your score was blah blah,
-			// are you ready for round two
-			// then click button yes
-			// when click that button this starts
-//click functions in jquery
-// trigger.addEventListener("click", toggleModal);
 $("#modal-button").on("click", (event) => {
 	event.preventDefault()
 	startRound();
@@ -155,20 +146,16 @@ $("#modal-button").on("click", (event) => {
 
 
 
-// closeButton.addEventListener("click", toggleModal);
-$(".close-button").on("click", toggleModal);
-// window.addEventListener("click", windowOnClick);
 
+$(".close-button").on("click", toggleModal);
 // check answer
 $('#answer').on('click', (event) => {//this gets the input 
 	event.preventDefault();//this prevents it 
-	// console.log('response');
 
 	const wordAnswer = $('#answerText').val();
 	console.log(wordAnswer);
 	
 	$('#answerText').text(wordAnswer);
-	// console.log(wordAnswer);
 
 	//if the word given is the same
 	if($('#wordBox').text() === wordAnswer) {
@@ -179,28 +166,19 @@ $('#answer').on('click', (event) => {//this gets the input
 		$('#score').text("Points: " + points)
 		getWord();
 		correctGuess();	
-		// countingWords();
 		// clear out the input
 		$('#answerText').val('')
 
 	} else { // user typed word incorrectly
 		points--;
 		wrongAnswer++;
-		// text.text("x")
 		text.append("x")
-		// $("#strike").text(textt);
-		
-		// x.text(wrongAnswer)
-		// alert(wrongAnswer + ' oh man');
 		gameOver();
 		
 		
 		$('#score').text("Points: " + points)
 		// print points in html
-		// console.log(points)
-		// alert("keep trying")
-		// getWord();
-		// countingWords();
+		
 		$('#answerText').val('')
 	}
 })
@@ -215,27 +193,4 @@ $('#name').on('click', (event) => {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-// .effect('explode')
-	
-	
-
-	// make counter to keep track of how many words so far
-
-	// each time you get aword 
-	// if the counter is 10
-	// next round
-	//otherwise increment the counter
-	// get the next word (what you already have)
-	///keep track of thw word
-	//point at
-
+			
